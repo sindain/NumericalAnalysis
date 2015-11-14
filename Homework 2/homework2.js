97519e780f937f683cedaa8aad86ebc6db9e78f0
sindain <sindain89@hotmail.com>
2015-11-13 22:34:32 -0600

    things

--- Homework 2/homework2.js
+++ Homework 2/homework2.js
@@ -57,14 +57,14 @@ function main(){
 		w = f1(c);
 
 		//ctx.fillText(n + " " + u + " " + v + " " + w,10, 10 + fontSize * (n + 2));
-		ctx.fillText(n, 10, 10 + fontSize * (n + row));
-		ctx.fillText(a, 10 + ctx.canvas.width / 6, 10 + fontSize * (n + row));
-		ctx.fillText(b, 10 + 2*ctx.canvas.width / 6, 10 + fontSize * (n + row));
-		ctx.fillText(c, 10 + 3*ctx.canvas.width / 6, 10 + fontSize * (n + row));
-		ctx.fillText(w, 10 + 4*ctx.canvas.width / 6, 10 + fontSize * (n + row));
-		
+		ctx.fillText(n, 10, 10 + fontSize * (row));
+		ctx.fillText(a, 10 + ctx.canvas.width / 6, 10 + fontSize * (row));
+		ctx.fillText(b, 10 + 2*ctx.canvas.width / 6, 10 + fontSize * (row));
+		ctx.fillText(c, 10 + 3*ctx.canvas.width / 6, 10 + fontSize * (row));
+		ctx.fillText(w, 10 + 4*ctx.canvas.width / 6, 10 + fontSize * (row));
+		row++
 		//Check and make sure we haven't found the solution
-		if(w == 0 || Math.abs(w) < delta || n >= nMax)
+		if(Math.abs(w) < delta || n >= nMax)
 			done = !done;
 
 		//If not done yet, update interval and continue
@@ -76,14 +76,53 @@ function main(){
 				a = c;
 			c = (a + b) / 2;
 			n++;
-		}
-	}
+		}//End if(!done)
+	} //End while(!done)
 
 //--------------------------------------  Newtons Method  ----------------------------------------
+	var x = 2.5;
+	var x1;
+	row += 3;
+	ctx.fillText("Newtons method",10, 10 + fontSize * row);
+	ctx.fillText("Initial Value x = " + x,ctx.canvas.width / 4, 10 + fontSize * row);
+	row ++;
+	ctx.fillText("Iteration n", 10, 10 + fontSize * row);
+	ctx.fillText("x(n)", 10 + ctx.canvas.width / 4, 10 + fontSize * row);
+	ctx.fillText("f(x(n))", 10 + 2*ctx.canvas.width / 4, 10 + fontSize * row);
+	ctx.fillText("f'(x(n))", 10 + 3*ctx.canvas.width / 4, 10 + fontSize * row);
+	ctx.moveTo(10, 10 + fontSize * row);
+	ctx.lineTo(10 + 5*ctx.canvas.width / 6, 12 + fontSize * row);
+	ctx.stroke();
+	row ++;
 
+	n = 0;
+	done = false;
+
+	while(!done){
+		a = f1(x);
+		b = f1p(x);
+		x1 = x - a/b;
+		ctx.fillText(n, 10, 10 + fontSize * (row));
+		ctx.fillText(x, 10 + ctx.canvas.width / 4, 10 + fontSize * (row));
+		ctx.fillText(a, 10 + 2*ctx.canvas.width / 4, 10 + fontSize * (row));
+		ctx.fillText(b, 10 + 3*ctx.canvas.width / 4, 10 + fontSize * (row));
+		row++
+
+		if(Math.abs(a) < delta || Math.abs(x1 - x) < delta || Math.abs(x1 - x) / x1 < delta)
+			done = !done;
+		if(!done){
+			x = x1;
+			n ++;
+		}
+		
+	}//End while(!done)
 	
 }
 
 function f1 (x){
 	return Math.pow(x,3) - 2 * Math.pow(x,2) + x - 3;
+}
+
+function f1p(x){
+	return 3*Math.pow(x,2) - 4 * x + 1;
 }
